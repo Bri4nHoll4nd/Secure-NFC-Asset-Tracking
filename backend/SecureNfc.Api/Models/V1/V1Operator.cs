7e9026784a7f47c257acc6ab5cd44ef8e77e5175
Bakk-e <jorgen-bakke@hotmail.com>
2026-07-07 19:59:29 +0200

    setup webapi and virtualnfcreader, and did some testing of the api

---
+++ backend/SecureNfc.Api/Models/V1/V1Operator.cs
@@ -0,0 +1,9 @@
+namespace SecureNfc.Api.Models.V1
+{
+    public class V1Operator
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
